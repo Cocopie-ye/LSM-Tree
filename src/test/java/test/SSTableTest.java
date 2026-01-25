df755d93298afb7978f8524964f10d0d2d74fb48
Cocopie-ye <anzhuo997@gmail.com>
2026-01-25 11:30:45 -0800

    chore: use a clean gitignore and remove idea files.

---
+++ src/test/java/test/SSTableTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SSTableTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void createSSTableFromMemtable() {
+  }
+
+  @Test
+  void get() {
+  }
+}
